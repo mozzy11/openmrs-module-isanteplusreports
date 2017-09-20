52ef2c12d78d530262ca230dc6d973c43b297d9b
Daniel Serkowski <dserkowski@soldevelo.com>
2017-09-20 11:43:28 +0200

    #290: Corrected adult indicator 1

--- api/src/main/resources/org/openmrs/module/isanteplusreports/sql/healthQualReports/retentionOfPatientsOnArt.sql
+++ api/src/main/resources/org/openmrs/module/isanteplusreports/sql/healthQualReports/retentionOfPatientsOnArt.sql
@@ -1,4 +1,3 @@
--- startDate -> SUBDATE(startDate, INTERVAL period MONTH);
 SELECT
     COUNT(
         DISTINCT CASE WHEN (
@@ -22,9 +21,15 @@ SELECT
             p.gender = 'M'
         ) THEN p.patient_id ELSE null END
 	) AS 'maleDenominator'
-FROM isanteplus.patient_status_ARV psa
-	INNER JOIN isanteplus.patient p ON psa.patient_id = p.patient_id
-    LEFT JOIN isanteplus.patient_dispensing pd ON psa.patient_id = pd.patient_id
-WHERE psa.start_date BETWEEN SUBDATE(:currentDate, INTERVAL :period MONTH) AND :currentDate
-	AND pd.drug_id IN (SELECT drug_id FROM isanteplus.arv_drugs)            -- ART treatment
-	AND (psa.dis_reason IS NULL OR psa.dis_reason NOT IN ('159','159492'))  -- not deceased and transfered
+FROM isanteplus.patient p
+    LEFT JOIN isanteplus.patient_dispensing pd ON p.patient_id = pd.patient_id
+WHERE pd.drug_id IN (SELECT drug_id FROM isanteplus.arv_drugs)  -- ART treatment
+	AND p.patient_id NOT IN (   -- Exclude deceased (159), transfer (159492)
+		SELECT discon.patient_id
+		FROM isanteplus.discontinuation_reason discon
+		WHERE discon.reason IN (159, 159492)
+	) AND p.patient_id IN (
+		SELECT patient_id
+        FROM isanteplus.patient_status_ARV
+        WHERE start_date BETWEEN SUBDATE(:currentDate, INTERVAL :period MONTH) AND :currentDate
+	);

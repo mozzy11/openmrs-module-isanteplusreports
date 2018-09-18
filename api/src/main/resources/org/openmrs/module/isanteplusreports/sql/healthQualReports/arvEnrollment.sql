82611930f11ba5b96fcd0a1efd54c65bf60e7f99
Wojciech Buława <wbulawa@soldevelo.com>
2018-09-18 17:13:53 +0200

    ITOS-302: Converted the adult 3. indicator from v2 to v3
    * Included discountinued patients in the query

--- api/src/main/resources/org/openmrs/module/isanteplusreports/sql/healthQualReports/arvEnrollment.sql
+++ api/src/main/resources/org/openmrs/module/isanteplusreports/sql/healthQualReports/arvEnrollment.sql
@@ -28,9 +28,4 @@ INNER JOIN isanteplus.health_qual_patient_visit pv ON p.patient_id = pv.patient_
 WHERE p.vih_status = '1' -- HIV+ patient
     AND pv.encounter_type IN ('1', '9') -- adult or pediatric first HIV visit
     AND pv.visit_date BETWEEN :startDate AND :endDate -- the date of first visit
-    AND p.patient_id NOT IN (
-        SELECT discon.patient_id
-        FROM isanteplus.discontinuation_reason discon
-        WHERE discon.reason IN ('159', '1667', '159492') -- not deceased, discontinued and transferred
-    )
     AND pv.age_in_years > 14;

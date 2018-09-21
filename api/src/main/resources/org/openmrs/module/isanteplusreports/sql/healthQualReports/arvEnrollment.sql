1d7592ee51ff6bea070640fc45222dae93dd8f37
areklalo <alalo@soldevelo.com>
2018-09-21 11:39:54 +0200

    ITOS-301: Changes after review

--- api/src/main/resources/org/openmrs/module/isanteplusreports/sql/healthQualReports/arvEnrollment.sql
+++ api/src/main/resources/org/openmrs/module/isanteplusreports/sql/healthQualReports/arvEnrollment.sql
@@ -28,4 +28,9 @@ INNER JOIN isanteplus.health_qual_patient_visit pv ON p.patient_id = pv.patient_
 WHERE p.vih_status = '1' -- HIV+ patient
     AND pv.encounter_type IN ('1', '9') -- adult or pediatric first HIV visit
     AND pv.visit_date BETWEEN :startDate AND :endDate -- the date of first visit
+    AND p.patient_id NOT IN (
+            SELECT discon.patient_id
+            FROM isanteplus.discontinuation_reason discon
+            WHERE discon.reason IN ('159', '159492') -- not deceased and transferred
+        )
     AND pv.age_in_years > 14;

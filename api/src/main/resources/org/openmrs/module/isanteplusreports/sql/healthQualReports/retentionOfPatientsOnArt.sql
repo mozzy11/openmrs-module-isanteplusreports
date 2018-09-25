dc7c1837dfa73a216aa8d1b9a7ed985fdf728167
areklalo <alalo@soldevelo.com>
2018-09-25 16:41:36 +0200

    ITOS-289: HIV+ Patients only

--- api/src/main/resources/org/openmrs/module/isanteplusreports/sql/healthQualReports/retentionOfPatientsOnArt.sql
+++ api/src/main/resources/org/openmrs/module/isanteplusreports/sql/healthQualReports/retentionOfPatientsOnArt.sql
@@ -37,6 +37,7 @@ FROM isanteplus.patient p
     LEFT JOIN isanteplus.patient_dispensing pd ON p.patient_id = pd.patient_id
     INNER JOIN isanteplus.patient_status_arv arv ON p.patient_id = arv.patient_id
 WHERE pd.drug_id IN (SELECT drug_id FROM isanteplus.arv_drugs)  -- ART treatment
+    AND p.vih_status = '1' -- HIV+ patient
     AND p.patient_id NOT IN (   -- Exclude discontinued (1667), transfer (159492)
         SELECT discon.patient_id
         FROM isanteplus.discontinuation_reason discon

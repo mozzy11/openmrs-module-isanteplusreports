382f4377d736de01b388bde2788e05d174906145
Ken Omondi <omondi.ken@gmail.com>
2020-05-19 20:39:50 +0300

    Corrected query (#63)
    Added missing PatientId column in the scripts union query

--- api/src/main/resources/org/openmrs/module/isanteplusreports/sql/fullDataExports/list_patients_eligible_for_charge_virale_controle.sql
+++ api/src/main/resources/org/openmrs/module/isanteplusreports/sql/fullDataExports/list_patients_eligible_for_charge_virale_controle.sql
@@ -21,7 +21,7 @@ GROUP BY p.patient_id
 
 UNION
 
-select DISTINCT p.st_id as 'Code ST',
+select DISTINCT p.patient_id AS 'Patient Id', p.st_id as 'Code ST',
 p.given_name as Prénom, p.family_name as Nom, p.telephone as Téléphone, pl.date_test_done AS 'Date du test',
 CASE WHEN (pl.test_id = 1305 AND pl.test_result = 1301) THEN 'Détectable'
 	ELSE

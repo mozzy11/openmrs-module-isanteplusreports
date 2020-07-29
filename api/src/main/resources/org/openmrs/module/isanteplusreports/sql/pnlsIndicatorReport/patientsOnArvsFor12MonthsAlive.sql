ecb0173caad3705eb7ba4025769420e0bfc6d5cc
mozzy11 <mozzymutesa@gmail.com>
2020-07-29 19:57:59 +0000

    update more indicator definitions

--- api/src/main/resources/org/openmrs/module/isanteplusreports/sql/pnlsIndicatorReport/patientsOnArvsFor12MonthsAlive.sql
+++ api/src/main/resources/org/openmrs/module/isanteplusreports/sql/pnlsIndicatorReport/patientsOnArvsFor12MonthsAlive.sql
@@ -1,6 +1,9 @@
 SELECT p.patient_id
-FROM isanteplus.patient p
-		  WHERE TIMESTAMPDIFF(MONTH,p.date_started_arv ,:endDate) >= 12 
-		  AND p.transferred_in = 0
-		  AND p.death_date IS  NULL
-		  AND p.voided = 0; 
+FROM isanteplus.patient_on_art pat ,isanteplus.patient_status_arv psa  , isanteplus.patient p  ,(SELECT ps.patient_id ,MAX(ps.date_started_status) AS max_staus_date FROM isanteplus.patient_status_arv ps WHERE ps.date_started_status BETWEEN :startDate AND :endDate  GROUP BY 1) B
+WHERE p.patient_id = pat.patient_id
+AND p.patient_id = psa.patient_id
+AND B.patient_id = p.patient_id
+AND ( TIMESTAMPDIFF(MONTH, pat.date_started_arv_for_transfered ,:endDate) >= 12 OR TIMESTAMPDIFF(MONTH, p.date_started_arv ,:endDate) >= 12 )
+AND p.patient_id NOT IN (SELECT psa.patient_id FROM isanteplus.patient_status_arv psa , isanteplus.patient p WHERE psa.patient_id = p.patient_id AND psa.id_status =2 AND p.date_started_arv <= :endDate)
+AND B.max_staus_date = psa.date_started_status
+AND psa.id_status <> 1 ;
--- api/src/main/resources/org/openmrs/module/isanteplusreports/sql/pnlsIndicatorReport/patientsOnArvsFor12MonthsInSight.sql
+++ api/src/main/resources/org/openmrs/module/isanteplusreports/sql/pnlsIndicatorReport/patientsOnArvsFor12MonthsInSight.sql
@@ -1,5 +1,6 @@
 SELECT p.patient_id
-FROM isanteplus.patient p
-		  WHERE TIMESTAMPDIFF(MONTH,p.date_started_arv ,:endDate) >= 12 
-		  AND p.transferred_in = 0
-		  AND p.voided = 0; 
+FROM isanteplus.patient_status_arv psa  , isanteplus.patient p
+WHERE p.patient_id = psa.patient_id
+AND p.transferred_in =0
+AND TIMESTAMPDIFF(MONTH, p.date_started_arv ,:endDate) >= 12 
+AND p.patient_id NOT IN (SELECT psa.patient_id FROM isanteplus.patient_status_arv psa , isanteplus.patient p WHERE psa.patient_id = p.patient_id AND psa.id_status =2 AND p.date_started_arv <= :endDate);

5e36c180c682c4a9caf582d345b35fbf46ae5680
mozzy11 <mozzymutesa@gmail.com>
2020-08-20 18:36:36 +0300

    update indicator definitons

--- api/src/main/resources/org/openmrs/module/isanteplusreports/sql/pnlsIndicatorReport/deadArvPatientsByHivIllnensses.sql
+++ api/src/main/resources/org/openmrs/module/isanteplusreports/sql/pnlsIndicatorReport/deadArvPatientsByHivIllnensses.sql
@@ -1,4 +1,5 @@
-SELECT pat.patient_id 
-    FROM isanteplus.patient_on_art pat
-    WHERE pat.deceased = 1
-    AND pat.cause_of_death_for_lost = "HIV" ;
+SELECT psa.patient_id
+   FROM isanteplus.patient_status_arv psa
+	    WHERE psa.patient_id IN (SELECT psa.patient_id FROM isanteplus.patient_status_arv psa WHERE TIMESTAMPDIFF(MONTH,psa.date_started_status ,:startDate) <=3 AND psa.id_status IN (6,8) ) 
+       AND psa.id_status = 2
+       AND psa.date_started_status  BETWEEN :startDate AND :endDate;
--- api/src/main/resources/org/openmrs/module/isanteplusreports/sql/pnlsIndicatorReport/lostPatientsResumedTreatment.sql
+++ api/src/main/resources/org/openmrs/module/isanteplusreports/sql/pnlsIndicatorReport/lostPatientsResumedTreatment.sql
@@ -1,4 +1,5 @@
-SELECT pat.patient_id 
-    FROM isanteplus.patient_on_art pat
-    WHERE pat.lost = 1
-    AND pat.resumed_arv_after_lost = 1 ;
+SELECT psa.patient_id
+   FROM isanteplus.patient_status_arv psa
+	   WHERE psa.patient_id IN (SELECT psa.patient_id FROM isanteplus.patient_status_arv psa WHERE TIMESTAMPDIFF(MONTH,psa.date_started_status ,:startDate) =1 AND psa.id_status =9 ) 
+       AND psa.id_status IN (6,8)
+       AND psa.date_started_status  BETWEEN :startDate AND :endDate;

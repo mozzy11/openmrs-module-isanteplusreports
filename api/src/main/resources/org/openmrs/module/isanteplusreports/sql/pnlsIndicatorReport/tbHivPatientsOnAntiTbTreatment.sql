406ce349bc49549ef4bfa8be7ac6da308f13c13e
mozzy11 <mozzymutesa@gmail.com>
2020-08-31 11:34:26 +0300

    update more definitions

--- api/src/main/resources/org/openmrs/module/isanteplusreports/sql/pnlsIndicatorReport/tbHivPatientsOnAntiTbTreatment.sql
+++ api/src/main/resources/org/openmrs/module/isanteplusreports/sql/pnlsIndicatorReport/tbHivPatientsOnAntiTbTreatment.sql
@@ -1,5 +1,6 @@
-    SELECT pa.patient_id
-   FROM isanteplus.patient_on_art pa	   
-	     WHERE  pa.date_started_anti_tb_treatment BETWEEN :startDate AND :endDate 
-		  AND pa.tb_status = "POSTIVE"
-		  AND pa.started_anti_tb_treatment = 1 ;
+SELECT p.patient_id
+FROM isanteplus.patient_on_art pat , isanteplus.patient p
+    WHERE p.patient_id = pat.patient_id
+	AND p.vih_status =1
+    AND pat.date_enrolled_on_tb_treatment IS NOT NUlL 
+	AND pat.date_enrolled_on_tb_treatment BETWEEN :startDate AND :endDate ;
